c45e4748a10a43a7fce12ed40c38087f613a279d
lucky150494 <60952482+lucky150494@users.noreply.github.com>
2020-02-27 19:53:36 +0700

    Update grab.py

--- grab.py
+++ grab.py
@@ -15,11 +15,20 @@ def save_ke_file(textnya):
 def grab_home():
 
 	hasil = requests.get('https://www.bibitbuahku.com/')
+
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
+# https://towardsdatascience.com/web-scraping-news-articles-in-python-9dd605799558
+# https://www.google.com/search?client=aff-maxthon-maxthon4&channel=t26&q=beatifulsoab%20python
 	
 	# page = requests.get('https://www.bibitbuahku.com/')
 	soup = BeautifulSoup(hasil.text, 'html.parser')
 	if page.status_code==200:
     		div = soup.find(id='cb-section-a')
+
+	articles = div.find_all('article')
+	for a in articles:
+   		single = a.find('h2')
+    		print(single.find('a').text)
 
 	# print(hasil.status_code) 
 	# print(hasil.text) 
